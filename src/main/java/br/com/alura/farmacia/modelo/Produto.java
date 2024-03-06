5cb71a97cdfe4840076fc3dbe51aede425ef2851
Fabiano <fabiano.ssantos79@gmail.com>
2024-03-05 22:17:12 -0300

    4 - Criar o arquivo de configuração - persistence.xml e classe Produto

---
+++ src/main/java/br/com/alura/farmacia/modelo/Produto.java
@@ -0,0 +1,42 @@
+package br.com.alura.farmacia.modelo;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private Fabricante fabricante;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+}
